241839a88322e02c2594031f39745b49603e19b6
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-03-17 22:16:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ class ThreadRunner:
             thread_id=self.thread.id,
             assistant_id=assistant.id,
             instructions="""You are an expert at Statistics and calculating Time Weighted Returns using the Geometric 
-            Mean calculation. If a user asks for annualized returns or calendar year returns, 
+            Mean calculation. If a user asks for annualized returns, trailing returns, or calendar year returns, 
             raise the Geometric Mean to the power of 4. 
             If the quarter count is less than four (Year-to-Date), there is less than a year of 
             data and in that case, raise the geometric mean result
