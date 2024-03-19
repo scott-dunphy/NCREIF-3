73d46a00d005e56f4da483060cd550ac8e5a55de
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-03-19 11:37:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,7 +145,7 @@ def run_query_and_display_results():
         # Clear or set a default message when there's no query
         st.session_state['results'] = "Please enter a query."
 
-st.title('AI NCREIF QUERY TOOL (With Analytics)')
+st.title('AI NCREIF QUERY TOOL w/ Analytics')
 
 # Text input for the query. The on_change function updates session state but doesn't directly display results.
 query = st.text_input("Enter your query:", key="query", on_change=run_query_and_display_results)
